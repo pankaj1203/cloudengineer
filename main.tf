b8b891784e1603927c095adbc3742515cb24c697
Narendra Sharma <narendra.sharma1982@hotmail.com>
2024-02-02 20:54:07 +0530

    Pushing Terraform code

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "AZ_RG_1" {
+  name     = "rg_az_weu_001"
+  location = "West Europe"
+}
---
+++ provide.tf
@@ -0,0 +1,24 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "<storage_account_name>"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = var.client_secret
+  tenant_id       = "10000000-0000-0000-0000-000000000000"
+  subscription_id = "20000000-0000-0000-0000-000000000000"
+}
