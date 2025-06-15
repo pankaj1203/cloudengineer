b76e7a5f71101cd033fa6c0ac4b5bcf26bc6e4bf
pankaj1203 <39940552+pankaj1203@users.noreply.github.com>
2025-06-15 11:51:19 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -20,5 +20,5 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id = "9229e7d9-5723-4406-943c-a0b864598b61"
+  subscription_id = "var.subscription_id"
 }
